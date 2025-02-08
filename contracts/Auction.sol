3ca7e72f5b5a4037ccdfe063cabe90532846e29c
Ahmed Ali <imahmedalibhatti@gmail.com>
2025-02-08 22:35:18 +0000

    feat: add a basic structure to start off Auction

---
+++ contracts/Auction.sol
@@ -0,0 +1,32 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.23;
+
+contract Auction {
+    struct Auction {
+        address token;
+        uint256 totalTokens;
+        uint256 startTime;
+        uint256 endTime;
+        bytes32 merkleRoot;
+        string ipfsHash;
+        bool isFinalized;
+    }
+
+    struct Bid {
+        address bidder;
+        uint256 quantity;
+        uint256 pricePerToken;
+    }
+
+    Auction public auction;
+    mapping(uint256 bidId => Bid bid) public bids;
+    uint256 public nextBidId;
+
+    // TODO: Add events
+
+    // TODO: Functions to add:
+    // startAuction -> only callable by the owner, starts the auction
+    // placeBid -> only callable by non-owner, places a bid
+    // endAuction -> callable by anyone, finalizes the auction
+    // claimTokens -> only callable by the winners to claim tokens and pay money in eth
+}
