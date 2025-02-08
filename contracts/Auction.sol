172b12ee82ef2d385ffbf4adaabc63b39a140ad3
Ahmed Ali <imahmedalibhatti@gmail.com>
2025-02-08 22:53:49 +0000

    feat: add basic logic for starting auction

--- contracts/Auction.sol
+++ contracts/Auction.sol
@@ -24,10 +24,50 @@ contract Auction is Ownable {
     mapping(uint256 bidId => BidState bid) public bids;
     uint256 public nextBidId;
 
+    error AuctionAlreadyExists();
+    error InvalidAuctionTime();
+    error InvalidTokenAddress();
+    error InvalidTotalTokens();
+
     // TODO: Add events
 
     // TODO: Functions to add:
     // startAuction -> only callable by the owner, starts the auction
+    function startAuction(
+        address token,
+        uint256 totalTokens,
+        uint256 startTime,
+        uint256 endTime
+    )
+        external
+        onlyOwner
+    {
+        if (auction.token != address(0)) {
+            revert AuctionAlreadyExists();
+        }
+        if (token == address(0)) {
+            revert InvalidTokenAddress();
+        }
+        if (endTime <= block.timestamp || startTime <= block.timestamp || endTime <= startTime) {
+            revert InvalidAuctionTime();
+        }
+        if (totalTokens == 0) {
+            revert InvalidTotalTokens();
+        }
+
+        auction = AuctionState({
+            token: token,
+            totalTokens: totalTokens,
+            startTime: startTime,
+            endTime: endTime,
+            merkleRoot: bytes32(0),
+            ipfsHash: "",
+            isFinalized: false
+        });
+
+        // TODO: Emit event
+    }
+
     // placeBid -> only callable by non-owner, places a bid
     // endAuction -> callable by anyone, finalizes the auction
     // claimTokens -> only callable by the winners to claim tokens and pay money in eth
