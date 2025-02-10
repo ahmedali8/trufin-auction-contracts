3ed89dbf5680f50200c52d7c8621d76fe61fe5c5
Ahmed Ali <imahmedalibhatti@gmail.com>
2025-02-10 22:56:17 +0000

    feat: implement a byte-packed struct for auction state

--- contracts/types/DataTypes.sol
+++ contracts/types/DataTypes.sol
@@ -1,12 +1,34 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.23;
 
+enum AuctionStatus {
+    STARTED,
+    MERKLE_SUBMITTED,
+    ENDED
+}
+
 struct AuctionState {
-    address token;
-    uint128 totalTokens;
-    uint40 startTime;
-    uint40 endTime;
-    bytes32 merkleRoot;
-    string ipfsHash;
-    bool isFinalized;
+    // slot 0
+    AuctionStatus status; // 1 byte
+    uint40 startTime; // 5 bytes
+    uint40 endTime; // 5 bytes
+    uint128 totalTokens; // 16 bytes
+    uint40 verificationDeadline; // 5 bytes - Timestamp when verification period ends
+    // slot 1
+    address token; // 20 bytes
+    bool isOwnerSlashed; // 1 bytes
+    uint8 hashFunction; // 1 byte - The hash function used.
+    uint8 size; // 1 byte - The size of the hash output.
+    // slot 2
+    bytes32 digest; // 32 bytes - The hash output.
+    // slot 3
+    bytes32 merkleRoot; // 32 bytes
+}
+
+struct BidState {
+    // slot 0
+    uint128 quantity; // 16 bytes
+    uint128 pricePerToken; // 16 bytes
+    // slot 1
+    address bidder; // 20 bytes
 }
