8cbc6f71eccc504696961beb0b5ee811fceac2e6
Ahmed Ali <imahmedalibhatti@gmail.com>
2025-02-10 17:54:57 +0000

    test: fix forge scripts

---
+++ scripts/foundry/DeployAuction.s.sol
@@ -0,0 +1,23 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity >=0.8.23;
+
+import { Auction } from "contracts/Auction.sol";
+
+import { BaseScript } from "./Base.s.sol";
+
+/// @dev See the Solidity Scripting tutorial:
+/// https://book.getfoundry.sh/tutorials/solidity-scripting
+contract DeployAuction is BaseScript {
+    function run(
+        address initialOwner,
+        address initialVerifier
+    )
+        public
+        virtual
+        broadcast
+        returns (Auction auction)
+    {
+        // deploy our contract
+        auction = new Auction(initialOwner, initialVerifier);
+    }
+}
