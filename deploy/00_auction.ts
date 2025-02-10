ee2445ecd7d77ea065dd6bd090f6bb721a0b8d12
Ahmed Ali <imahmedalibhatti@gmail.com>
2025-02-10 17:57:51 +0000

    feat: add deployment script for auction

--- deploy/00_auction.ts
+++ deploy/00_auction.ts
@@ -1,6 +1,4 @@
-import { time } from "@nomicfoundation/hardhat-network-helpers";
-import type { BigNumberish } from "ethers";
-import type { DeployFunction, DeployResult } from "hardhat-deploy/types";
+import type { Address, DeployFunction, DeployResult } from "hardhat-deploy/types";
 import type { HardhatRuntimeEnvironment } from "hardhat/types";
 
 import { preDeploy } from "../utils/contracts";
@@ -9,32 +7,24 @@ import { verifyContract } from "../utils/verify";
 const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
   const { getNamedAccounts, getChainId, deployments } = hre;
   const { deploy } = deployments;
-  const { deployer } = await getNamedAccounts();
+  const { owner, verifier } = await getNamedAccounts();
   const chainId = await getChainId();
 
-  const ONE_YEAR_IN_SECS = time.duration.years(1);
-  const ONE_WEI = "1";
+  type ConstructorParams = [Address, Address];
+  const args: ConstructorParams = [owner, verifier];
 
-  const currentTimestampInSeconds = await time.latest();
-  const unlockTime = currentTimestampInSeconds + ONE_YEAR_IN_SECS;
-  const lockedAmount = ONE_WEI;
-
-  type ConstructorParams = [BigNumberish];
-  const args: ConstructorParams = [unlockTime];
-
-  await preDeploy(deployer, "Lock");
-  const deployResult: DeployResult = await deploy("Lock", {
+  await preDeploy(owner, "Auction");
+  const deployResult: DeployResult = await deploy("Auction", {
     // Learn more about args here: https://www.npmjs.com/package/hardhat-deploy#deploymentsdeploy
-    from: deployer,
+    from: owner,
     args: args,
     log: true,
-    value: lockedAmount.toString(),
     // waitConfirmations: 5,
   });
 
   // You don't want to verify on localhost
   if (chainId !== "31337" && chainId !== "1337") {
-    const contractPath = `contracts/Lock.sol:Lock`;
+    const contractPath = `contracts/Auction.sol:Auction`;
     await verifyContract({
       contractPath: contractPath,
       contractAddress: deployResult.address,
@@ -44,5 +34,5 @@ const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
 };
 
 export default func;
-func.id = "deploy_lock";
-func.tags = ["Lock"];
+func.id = "deploy_auction";
+func.tags = ["Auction"];
