0b3b4201cf060d98c33d8699f25d41bc4018af5f
Ahmed Ali <imahmedalibhatti@gmail.com>
2025-02-11 12:56:07 +0000

    test: update new errors

--- test/shared/errors.ts
+++ test/shared/errors.ts
@@ -1,5 +1,29 @@
-export enum Errors {
-  Lock_CallerNotOwner = "Lock_CallerNotOwner",
-  Lock_CannotWithdrawYet = "Lock_CannotWithdrawYet",
-  Lock_UnlockTimeShouldBeInTheFuture = "Lock_UnlockTimeShouldBeInTheFuture",
+export enum OwnableErrors {
+  OwnableUnauthorizedAccount = "OwnableUnauthorizedAccount",
+}
+
+export enum ERC20TokenErrors {
+  ERC20InsufficientAllowance = "ERC20InsufficientAllowance",
+}
+
+export enum AuctionErrors {
+  InvalidAddress = "InvalidAddress",
+  ZeroTotalTokens = "ZeroTotalTokens",
+  InvalidAuctionStatus = "InvalidAuctionStatus",
+  AuctionExists = "AuctionExists",
+  CanOnlySubmitOnce = "CanOnlySubmitOnce",
+  InvalidSecurityDeposit = "InvalidSecurityDeposit",
+  InvalidPricePerToken = "InvalidPricePerToken",
+  InvalidAuctionTimeParams = "InvalidAuctionTimeParams",
+  InvalidBidQuantity = "InvalidBidQuantity",
+  InvalidBidPrice = "InvalidBidPrice",
+  BidDoesNotExist = "BidDoesNotExist",
+  OwnerCannotPlaceBids = "OwnerCannotPlaceBids",
+  InvalidMerkleRoot = "InvalidMerkleRoot",
+  InvalidMerkleProof = "InvalidMerkleProof",
+  InvalidMultiHash = "InvalidMultiHash",
+  VerificationPeriodNotOver = "VerificationPeriodNotOver",
+  VerificationWindowExpired = "VerificationWindowExpired",
+  OnlyVerifierCanResolveDispute = "OnlyVerifierCanResolveDispute",
+  EthTransferFailed = "EthTransferFailed",
 }
