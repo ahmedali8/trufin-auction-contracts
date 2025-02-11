bdf00130a0894d243340f4a7861d0c4d8b516dc2
Ahmed Ali <imahmedalibhatti@gmail.com>
2025-02-11 00:18:05 +0000

    feat: add errors

--- contracts/libraries/Errors.sol
+++ contracts/libraries/Errors.sol
@@ -6,4 +6,28 @@ pragma solidity ^0.8.22;
 /// @notice Library to manage error messages for the LaunchpadV3 contracts.
 library Errors {
     error AddressZero();
+
+    error AuctionAlreadyExists();
+    error InvalidAuctionTime();
+    error InvalidTokenAddress();
+    error InvalidTotalTokens();
+    error AuctionNotActive();
+    error InvalidBidQuantity();
+    error InvalidBidPrice();
+    error OwnerCannotPlaceABid();
+    error AuctionStillActive();
+    error AuctionAlreadyFinalized();
+    error InvalidIPFSHash();
+    error InvalidMerkleRoot();
+    error AuctionNotFinalized();
+    error BidDoesNotExist();
+    error TokensAlreadyClaimed();
+    error InvalidProof();
+    error EthTransferFailed();
+    error ZeroAddress();
+    error InvalidSecurityDeposit();
+    error VerificationPeriodNotOver();
+    error OnlyVerifierCanResolveDispute();
+    error AuctionMustHaveAnInitialMerkleRoot();
+    error VerificationWindowExpired();
 }
